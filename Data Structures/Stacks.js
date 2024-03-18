bd2b9463f4dab8a90f73de83892d0b0f58b4168e
Starnoh254 <stajohn697@gmail.com>
2024-03-18 22:47:07 +0300

    Stack data structure

---
+++ Data Structures/Stacks.js
@@ -0,0 +1,9 @@
+/* a Stack follows the FiFo rule 
+(First In First Out) which means that the element added last will be removed first.
+*/
+
+let stack = [1,2,3,4,5,6,7,8,9,]
+console.log(stack.pop())
+console.log(stack.push(10))
+
+console.log(stack)
