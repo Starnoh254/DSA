41d153cfcb3c20bf6916548e9ae376c1f060f4eb
Starnoh254 <stajohn697@gmail.com>
2024-03-19 23:03:43 +0300

    queue data structure

---
+++ Data Structures/Queue2.js
@@ -0,0 +1,50 @@
+class Queue {
+  constructor() {
+    this.items = {};
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(element) {
+    this.items[this.rear] = element;
+    this.rear++;
+  }
+
+  dequeue() {
+    const item = this.items[this.front];
+    delete this.items[this.front];
+    this.front++;
+    return item;
+  }
+
+  isEmpty() {
+    return this.rear - this.front === 0;
+  }
+
+  peek() {
+    return this.items[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+
+  print() {
+    console.log(this.items);
+  }
+}
+
+const queue = new Queue()
+console.log(queue.isEmpty())
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+
+console.log(queue.size())
+queue.print()
+
+console.log(queue.dequeue())
+
+console.log(queue.peek())
+
+queue.print()
---
+++ Problems/binary_search.js
@@ -0,0 +1,3 @@
+ 
+
+console.log(binary_search([1,2,3,4,5,6,7,8,9] , 1))
