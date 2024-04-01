27d8472c014b3a3cd7df79324e4134ae55ac4a6f
Starnoh254 <stajohn697@gmail.com>
2024-04-01 17:39:49 +0300

    solved a bug

--- Problems/isWinner.js
+++ Problems/isWinner.js
@@ -1,9 +1,7 @@
 function scores(player) {
   let score = 0;
   for (let i = 0; i < player.length; i++) {
-    if (player[i - 1] == undefined || player[i - 2] == undefined) {
-      score += player[i];
-    } else if (player[i - 1] == 10 || player[i - 2] == 10) {
+    if (player[i - 1] == 10 || player[i - 2] == 10) {
       score += player[i] * 2;
     } else {
       score += player[i];
@@ -15,8 +13,8 @@ function scores(player) {
 function isWinner(player1, player2) {
   let sumOfPlayer1 = scores(player1);
   let sumOfPlayer2 = scores(player2);
-  console.log(sumOfPlayer1)
-  console.log(sumOfPlayer2)
+  console.log(sumOfPlayer1);
+  console.log(sumOfPlayer2);
   if (sumOfPlayer1 > sumOfPlayer2) {
     return 1;
   } else if (sumOfPlayer1 < sumOfPlayer2) {
@@ -26,4 +24,4 @@ function isWinner(player1, player2) {
   }
 }
 
-console.log(isWinner([4, 10, 7, 9], [6, 5, 2, 3]));
+console.log(isWinner([10, 2, 2, 3], [3, 8, 4, 5]));
