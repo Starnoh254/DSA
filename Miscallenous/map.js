06f11eec6a5523d4134496811980c9841ad4031d
Starnoh254 <stajohn697@gmail.com>
2024-04-20 18:28:51 +0300

    Map data structure

---
+++ Miscallenous/map.js
@@ -0,0 +1,12 @@
+const newMap = new Map();
+
+// Adding key - value pairs to the map
+newMap.set("Name", "John");
+newMap.set("Age", 30);
+newMap.set("Profession", "Software Engineer");
+
+
+console.log(newMap.get("Profession"))
+for (let [key, value] of newMap.entries()) {
+  console.log(`${key} : ${value}`);
+}
